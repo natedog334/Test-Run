d56d91431b9207857ebafe9f7cd09ab8a78acbf3
natedog334 <58520743+natedog334@users.noreply.github.com>
2020-10-05 22:41:18 -0400

    added spinner, no effect on calculations

--- app/src/main/java/com/example/testrun/MainActivity.java
+++ app/src/main/java/com/example/testrun/MainActivity.java
@@ -2,18 +2,28 @@ package com.example.testrun;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Spinner;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Spinner unitSpinner = findViewById(R.id.unitSpinner);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.units, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        unitSpinner.setAdapter(adapter);
+        unitSpinner.setOnItemSelectedListener(this);
 
         Button addButton = (Button) findViewById(R.id.calcButton);
         addButton.setOnClickListener(new View.OnClickListener() {
@@ -21,6 +31,7 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View v) {
                 EditText timeEditText = (EditText) findViewById(R.id.timeEditText);
                 EditText distanceNumEditText = (EditText) findViewById(R.id.distanceEditText);
+
                 TextView resultTextView = (TextView) findViewById(R.id.resultTextView);
 
                 String time = timeEditText.getText().toString();
@@ -48,4 +59,14 @@ public class MainActivity extends AppCompatActivity {
     }
 
 
+    @Override
+    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+        String text = parent.getItemAtPosition(position).toString();
+        Toast.makeText(parent.getContext(), text, Toast.LENGTH_SHORT);
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> parent) {
+
+    }
 }
